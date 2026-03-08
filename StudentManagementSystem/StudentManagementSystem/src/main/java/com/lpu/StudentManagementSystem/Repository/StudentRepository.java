be45039884654dd8591d6e3c12d72f2191bba8bf
Jomaina Hafiz Ahmed <118652968+JomainaAhmed@users.noreply.github.com>
2026-03-09 00:31:51 +0530

    Delete StudentManagementSystem/StudentManagementSystem directory

--- StudentManagementSystem/StudentManagementSystem/src/main/java/com/lpu/StudentManagementSystem/Repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lpu.StudentManagementSystem.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.lpu.StudentManagementSystem.entity.*;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
